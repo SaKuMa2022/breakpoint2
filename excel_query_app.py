15743b18558aff7495de14d0f82cf74e10007ea3
SaKuMa2022 <114770923+SaKuMa2022@users.noreply.github.com>
2024-04-03 09:58:24 -0400

    Breakpoint_app.py

--- excel_query_app.py
+++ excel_query_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    st.title("Excel Query App")
+    st.title("Breakpoint Information from your own data")
     
     # Upload Excel file
     uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
